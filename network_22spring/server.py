71945d7b2e0dab211cab73633c7a46dcc7006cc3
Martina Lee <martinalee0528@gmail.com>
2022-03-31 23:58:18 +0900

    file list 출력

--- network_22spring/server.py
+++ network_22spring/server.py
@@ -23,10 +23,14 @@ server.listen(5)
 while True:
     client, address = server.accept()
     data = client.recv(65535)
-    data_temp = data.decode().split()
+    data_temp = data.decode(encoding="UTF8").split("\r\n")
+    data_dict = {}
+    for d in data_temp:
+        if ":" in d:
+            temp = d.split(":")
+            data_dict[temp[0]] = temp[1:]
     # https://kentakang.com/133
-    request_method = data_temp[0]
-    page = data_temp[1]
+    request_method, page, _ = data_temp[0].split()
     if page == "/" or page == "/index.html":
         if request_method == "GET":
             filename = "./index.html"
@@ -56,15 +60,35 @@ while True:
             )
             client.send(str.encode("\r\n"))
     elif page == "/storage" or page == "/storage.html":
-        if "Cookie:" in data_temp:
+        if data_dict.get("Cookie"):
+            form_data = data_dict["Cookie"][0]
+            user = form_data.split(";")[0]
+            user = user.split("=")[1]
             if request_method == "GET":
                 filename = "./storage.html"
                 read_html(filename, client)
+            elif request_method == "POST":
+                for d in data_temp:
+                    if "submitted_file" in d:
+                        d_temp = d.split(";")[-1]
+                        filename = d_temp.split("=")[1]
+                if not os.path.isdir("./{}".format(user)):
+                    os.mkdir("./{}".format(user))
         else:
             client.send(str.encode("HTTP/1.1 302 redirect\n"))
             client.send(str.encode("Location: {}\n".format("./index.html")))
+    elif page == "/storage/files":
+        form_data = data_dict["Cookie"][0]
+        user = form_data.split(";")[0]
+        user = user.split("=")[1]
+        path = os.path.abspath(user)
+        file_list = ",".join(os.listdir(path))
+        client.send(str.encode("HTTP/1.1 200 OK\n"))
+        client.send(str.encode("Content-Type: text/plain\n"))
+        client.send(str.encode("\r\n"))
+        client.send(str.encode(f"{file_list}\n"))
     elif page == "/cookie" or page == "/cookie.html":
-        if "Cookie:" in data_temp:
+        if "Cookie" in data_temp:
             filename = "./cookie.html"
             read_html(filename, client)
         else:
